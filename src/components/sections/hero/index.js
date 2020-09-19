b85e3e5b0839ae90e62f1533cb0f7d6debe4d1c1
akramakh <akram.icode@gmail.com>
2020-09-19 13:52:28 +0300

    edit 'hero' section to read the date from firebase

--- src/components/sections/hero/index.js
+++ src/components/sections/hero/index.js
@@ -1,21 +1,56 @@
-import React from 'react';
-import Bg from '../../../assets/images/bg.jpg';
+import React, {Component} from 'react';
+import {FirebaseDB} from '../../../firebaseConfig';
 import './index.scss';
 
-export default function Hero() {
-  return (
-    <section id='hero'>
-      <div class='bg' id='hero_bg'>
-        <div class='overlay'></div>
-        <img src={Bg} alt='background' />
-      </div>
-      <div class='greeting-container' id='greeting'>
-        <h1 class='question'>I'm Akram</h1>
-        <b class='answer'>Web Developer & UI/UX Designer</b>
-        <a href='#about' class='btn btn-primary explore-me'>
-          explore me
-        </a>
-      </div>
-    </section>
-  );
+const initData = {
+  action_button: {
+    text: 'Explore Me',
+    url: '#about',
+    style: '{}',
+  },
+  bg_image: 'https://images.unsplash.com/photo-1521171338749-b3a9f9ad7f34',
+  greeting_text: "I'm Akram",
+  description: 'Web Developer & UI/UX Designer',
+};
+
+export default class Hero extends Component {
+  state = {...initData};
+  headerRef = FirebaseDB.ref('sections/hero/');
+
+  componentDidMount() {
+    this.headerRef.on('value', (snapshot) => {
+      if (snapshot && snapshot.exists()) {
+        const data = snapshot.val();
+        let {action_button, bg_image, description, greeting_text} = data;
+        this.setState((state, props) => {
+          return {
+            ...state,
+            bg_image,
+            action_button,
+            description,
+            greeting_text,
+          };
+        });
+      }
+    });
+  }
+
+  render = () => {
+    const {bg_image, action_button, description, greeting_text} = this.state;
+    return (
+      <section id='hero'>
+        <div class='bg' id='hero_bg'>
+          <div class='overlay'></div>
+          <img src={bg_image} alt='background' />
+        </div>
+        <div class='greeting-container' id='greeting'>
+          <h1 class='question'>{greeting_text}</h1>
+          <b class='answer'>{description}</b>
+          <a href={action_button.url} class='btn btn-primary explore-me'>
+            {action_button.text}
+          </a>
+        </div>
+      </section>
+    );
+  };
 }

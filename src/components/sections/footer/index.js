d1d4a4be6ab75aef254b6d44f9dcf30d8cfaa8ca
akramakh <akram.icode@gmail.com>
2020-09-21 19:45:19 +0300

    edit 'footer' section to read data from firebase database

--- src/components/sections/footer/index.js
+++ src/components/sections/footer/index.js
@@ -1,4 +1,5 @@
-import React from 'react';
+import React, {useState, useEffect} from 'react';
+import {Tooltip} from 'antd';
 import {
   FaFacebookF,
   FaInstagram,
@@ -7,73 +8,77 @@ import {
   FaWhatsapp,
   FaBehance,
 } from 'react-icons/fa';
+import {FirebaseDB} from '../../../firebaseConfig';
 
 import './style.scss';
 
+const initData = {
+  social_links: [],
+  username: '',
+  website_type: 'Personal Portfolio',
+};
+
+const iconsMap = {
+  FaFacebookF: <FaFacebookF />,
+  FaInstagram: <FaInstagram />,
+  FaTwitter: <FaTwitter />,
+  FaGithub: <FaGithub />,
+  FaWhatsapp: <FaWhatsapp />,
+  FaBehance: <FaBehance />,
+};
+
 export default function Footer() {
   const year = new Date().getFullYear();
+
+  const [username, setUsername] = useState(initData.username);
+  const [websiteType, setWebsiteType] = useState(initData.website_type);
+  const [socialLinks, setSocialLinks] = useState(initData.social_links);
+
+  useEffect(() => {
+    const footerRef = FirebaseDB.ref('sections/footer/');
+    footerRef.on('value', (snapshot) => {
+      if (snapshot && snapshot.exists()) {
+        const data = snapshot.val();
+        let {username, website_type, social_links} = data;
+        const keys = Object.keys(social_links);
+        const links_list = keys.map((key) => social_links[key]);
+        setUsername(username);
+        setWebsiteType(website_type);
+        setSocialLinks(links_list);
+      }
+    });
+  }, []);
+
+  const renderLinks = () => {
+    const data = socialLinks.map((link) => {
+      return (
+        <Tooltip placement='top' title={link.title}>
+          <a
+            href={link.url}
+            title={link.title}
+            class={link.class_name}
+            target='_blank'
+          >
+            {iconsMap[link.icon]}
+          </a>
+        </Tooltip>
+      );
+    });
+    return <p class='social'>{data}</p>;
+  };
   return (
     <div>
       <footer>
         <div class='container cms-container'>
           <div class='row copyright'>
             <div class='col-md-3'>
-              <p class='copyright roboto'>&copy;{year} | Akram AKH</p>
-            </div>
-            <div class='col-md-6'>
-              <p class='social'>
-                <a
-                  href='http://www.fb.com/akramabukhousa'
-                  title='@akramabukhousa'
-                  class='facebook'
-                  target='_blank'
-                >
-                  <FaFacebookF />
-                </a>
-                <a
-                  href='http://www.twitter.com/akramabukhousa'
-                  title='@akramabukhousa'
-                  class='twitter'
-                  target='_blank'
-                >
-                  <FaTwitter />
-                </a>
-                <a
-                  href='http://www.instagram.com/akramabukhousa'
-                  title='@akramabukhousa'
-                  class='instagram'
-                  target='_blank'
-                >
-                  <FaInstagram />
-                </a>
-                <a
-                  href='http://github.com/akramakh'
-                  title='+970595527040'
-                  class='gplus'
-                  target='_blank'
-                >
-                  <FaGithub />
-                </a>
-                <a
-                  href='http://www.wa.me/970595527040'
-                  title='+970595527040'
-                  class='gplus'
-                  target='_blank'
-                >
-                  <FaWhatsapp />
-                </a>
-                <a
-                  href='http://www.behance.net/akramakh'
-                  title='@akramabukhousa'
-                  class='email'
-                  target='_blank'
-                >
-                  <FaBehance />
-                </a>
+              <p class='copyright roboto'>
+                &copy;{year} | {username}
               </p>
             </div>
+            <div class='col-md-6'>{renderLinks()}</div>
             <div class='col-md-3'>
-              <p class='credit roboto'>Personal Website</p>
+              <p class='credit roboto'>{websiteType}</p>
             </div>
           </div>
         </div>
